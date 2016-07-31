d7f4f24a981c51775850a5ceb58ba7d9cc00d1f6
Dzianis Dzetko <dzianis.dzetko@gmail.com>
2016-07-31 09:44:05 +0200

    add admin

--- source/js/main.js
+++ source/js/main.js
@@ -481,5 +481,24 @@ $(window).scroll(function(){
 
 
 
-//      admin       //
+/* =================================*/
+/*               Admin:             */
+/* =================================*/
 
+var adminTabs = function(control_selector, content_selector){
+  $(control_selector).on("click", function(){
+    var index = $(this).index(),
+      content = $(content_selector);
+
+    content.eq(index)
+      .add(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  });
+};
+
+
+if($(".admin-page").length){
+    adminTabs(".tabs-control_item", ".tabs-content_item");
+};
